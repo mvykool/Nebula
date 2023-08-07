5af2eb90c90605f538fdc312b0defa3486ca4a9e
maicol hernandez <maykolandres4@gmail.com>
2023-08-06 20:48:23 -0500

    added more stuff

--- c#.cs
+++ c#.cs
@@ -123,4 +123,113 @@ class Program
     }
 }
 
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
+namespace NoteTakingApp
+{
+    public partial class MainForm : Form
+    {
+        private List<string> notes = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string note = noteTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(note))
+            {
+                notes.Add(note);
+                noteTextBox.Clear();
+                MessageBox.Show("Note added successfully!", "Note Taking App", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void viewButton_Click(object sender, EventArgs e)
+        {
+            if (notes.Count == 0)
+            {
+                MessageBox.Show("No notes to display.", "Note Taking App", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string notesText = string.Join("\n", notes);
+            MessageBox.Show(notesText, "Your Notes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
+
+using System;
+using System.Collections.Generic;
+
+class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
+
+class ShoppingCart
+{
+    private List<Product> items = new List<Product>();
+
+    public void AddItem(Product product)
+    {
+        items.Add(product);
+    }
+
+    public double CalculateTotal()
+    {
+        double total = 0;
+        foreach (Product item in items)
+        {
+            total += item.Price;
+        }
+        return total;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShoppingCart cart = new ShoppingCart();
+
+        Product apple = new Product("Apple", 0.5);
+        Product banana = new Product("Banana", 0.3);
+        Product laptop = new Product("Laptop", 1000.0);
+
+        cart.AddItem(apple);
+        cart.AddItem(banana);
+        cart.AddItem(laptop);
+
+        Console.WriteLine("Items added to cart:");
+        foreach (Product item in cart.Items)
+        {
+            Console.WriteLine($"{item.Name} - ${item.Price}");
+        }
+
+        double total = cart.CalculateTotal();
+        Console.WriteLine($"Total: ${total}");
+    }
+}
