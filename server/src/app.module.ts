e625ecd515d3e2155a5c64c560f4ee8be9ad6f4e
mvykool <maykolandres4@gmail.com>
2024-08-12 02:32:28 -0500

    setup db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,28 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { PagesModule } from './pages/pages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [UsersModule, ProjectsModule, PagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'mvykol',
+      password: 'maicol123',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ProjectsModule,
+    PagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  private dataSource: DataSource;
+}
