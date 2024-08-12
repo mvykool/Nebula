d12ae959f931a25c017ae4cac1f7f4cd8bab07a9
mvykool <maykolandres4@gmail.com>
2024-08-12 00:28:35 -0500

    added projects and pages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { PagesModule } from './pages/pages.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProjectsModule, PagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
