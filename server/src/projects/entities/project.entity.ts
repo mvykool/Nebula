0c73b02635797f28b3da623e9d065a5e27d37e5e
mvykool <maykolandres4@gmail.com>
2024-08-16 01:56:29 -0500

    started project entity

--- server/src/projects/entities/project.entity.ts
+++ server/src/projects/entities/project.entity.ts
@@ -1,1 +1,16 @@
-export class Project {}
+import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
+
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 30 })
+  name: string;
+
+  @Column({ type: 'text', length: 255 })
+  description: string;
+
+  @Column({ type: 'varchar', length: 40 })
+  created: Date;
+}
