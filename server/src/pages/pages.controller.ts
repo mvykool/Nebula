6378ed435d9be82005e9bb8e4b70d87c5560e643
mvykool <maykolandres4@gmail.com>
2024-08-18 16:49:45 -0500

    edited controller

--- server/src/pages/pages.controller.ts
+++ server/src/pages/pages.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PagesService } from './pages.service';
 import { CreatePageDto } from './dto/create-page.dto';
 import { UpdatePageDto } from './dto/update-page.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('pages')
+@UseGuards(AuthGuard)
 export class PagesController {
   constructor(private readonly pagesService: PagesService) {}
 
   @Post()
   create(@Body() createPageDto: CreatePageDto) {
-    return this.pagesService.create(createPageDto);
+    return this.pagesService.createPage(createPageDto);
   }
 
   @Get()
