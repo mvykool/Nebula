1243b734a8e35b48029959c6f77183b04a3a2011
mvykool <maykolandres4@gmail.com>
2024-08-18 16:06:40 -0500

    project service

--- server/src/projects/dto/create-project.dto.ts
+++ server/src/projects/dto/create-project.dto.ts
@@ -1,1 +1,13 @@
-export class CreateProjectDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateProjectDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsString()
+  cover?: string;
+
+  @IsString()
+  description?: string;
+}
--- server/src/projects/projects.service.ts
+++ server/src/projects/projects.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {}
+  create(createProjectDto: CreateProjectDto, user: User): Promise<Project> {
+    const project: Project = new Project();
+    project.name = createProjectDto.name;
+    project.cover = createProjectDto.cover;
+    project.description = createProjectDto.description;
+    project.owner = user;
+    return this.projectRepository.save(project);
   }
 
-  findAll() {
-    return `This action returns all projects`;
+  findAll(): Promise<Project[]> {
+    return this.projectRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
+  findOne(id: number): Promise<Project> {
+    return this.projectRepository.findOneBy({ id });
   }
 
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
+  async update(id: number, updateProject: UpdateProjectDto): Promise<Project> {
+    await this.projectRepository.update(id, updateProject);
+    return this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} project`;
+  remove(id: number): Promise<{ affected?: number }> {
+    return this.projectRepository.delete(id);
   }
 }
