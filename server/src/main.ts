8166cc287aa8de96d6b144fa3c350003c8a7759e
mvykool <maykolandres4@gmail.com>
2024-08-20 01:59:14 -0500

    swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //swagger docs
+  const configuration = new DocumentBuilder()
+    .setTitle('Nebula')
+    .setDescription('API for nebula')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configuration);
+
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 
