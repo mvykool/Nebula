3edf8421289304ae335e98577ec7a53e8f31de36
mvykool <maykolandres4@gmail.com>
2024-08-21 20:29:30 -0500

    got rid of unnecessary controllers and service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { PagesModule } from './pages/pages.module';
@@ -38,7 +36,5 @@ import { ThrottlerModule } from '@nestjs/throttler';
     PagesModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
