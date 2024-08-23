19b361f63e969dedc843424ba38d747ace850f24
mvykool <maykolandres4@gmail.com>
2024-08-22 19:15:11 -0500

    added box icons

--- client/nebula/src/views/home.tsx
+++ client/nebula/src/views/home.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>hello world</div>
+    <div>
+      <i className='bx bx-user'></i>
+      <i className='bx bxs-user'></i>
+      <i className='bx bxl-facebook-square'></i>
+    </div>
   )
 }
 
