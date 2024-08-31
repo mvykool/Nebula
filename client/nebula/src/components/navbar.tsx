47df75acb222972b8fff7ccca084e1487c277ab0
mvykool <maykolandres4@gmail.com>
2024-08-30 20:12:09 -0500

    added logic for theming

--- client/nebula/src/components/navbar.tsx
+++ client/nebula/src/components/navbar.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import UserAndLogOut from "./userAndLogOut"
 
 const Navbar = () => {
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
 
   return (
     <div className="w-full h-16 flex justify-around items-center">
@@ -13,14 +30,18 @@ const Navbar = () => {
 
       {/*Main button*/}
       <div className=" flex items-center gap-3">
-        <button className="px-2 py-1 bg-purple-200 rounded-md">Projects</button>
+        <button className="px-2 py-1 bg-purple-200 dark:bg-red-200 rounded-md">Projects</button>
       </div>
 
 
       {/*user info and logout*/}
       <div className="flex items-center gap-3 w-[205px]">
         <ul className="flex gap-3 mr-4">
-          <li><i className='bx bx-moon text-xl cursor-pointer'></i></li>
+          <li>
+            <button type="button" onClick={toggleTheme}>
+              <i className='bx bx-moon text-xl cursor-pointer'></i>
+            </button>
+          </li>
           <li><i className='bx bx-bell text-xl cursor-pointer' ></i></li>
         </ul>
 
