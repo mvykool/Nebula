da5fd2d21736d3608c7e7211875f93bec0be2251
mvykool <maykolandres4@gmail.com>
2024-09-14 12:18:35 -0500

    started with sidebar

--- client/nebula/src/components/sidebar.tsx
+++ client/nebula/src/components/sidebar.tsx
@@ -1,12 +1,24 @@
 import { useAuth } from "../hooks/authProvider";
+
 const Sidebar = () => {
+  const { user, defaultPfp } = useAuth();
+
   return (
-    <div className="border border-white left-0 top-0 w-2/12 h-screen">
+    <div className="border border-white left-0 top-0 w-2/12 h-screen text-black dark:text-white">
       {/*OWNER INFO*/}
-      <div></div>
+      <div className="flex gap-3 mt-5 items-end p-8">
+        <img
+          src={user?.picture || defaultPfp}
+          className="h-14 w-14 rounded-md"
+          alt="user-picture"
+        />
+        <p className="font-semibold text-xl">{user?.username}</p>
+      </div>
 
       {/*PROJECT SECTIONS*/}
-      <div></div>
+      <div className="p-8 mt-5">
+        <span className="font-bold">Pages:</span>
+      </div>
     </div>
   );
 };
