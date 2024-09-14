c9022f8b5008ca22765b784093269e16f3eb6ac2
mvykool <maykolandres4@gmail.com>
2024-09-14 07:48:18 -0500

    created project single page

---
+++ client/nebula/src/views/project/project.tsx
@@ -0,0 +1,21 @@
+const Project = () => {
+  return (
+    <>
+      {/*SIDE BAR*/}
+      <div>sidebar</div>
+
+      {/* MAIN CONTENT */}
+      <div>
+        <img src="#" alt="cover-image" />
+
+        {/*TITLE*/}
+        <h3>title</h3>
+
+        {/*CONTENT*/}
+        <p>content</p>
+      </div>
+    </>
+  );
+};
+
+export default Project;
