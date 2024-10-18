a5c40ae251971f9f762f4c57c4e7296029fbfcb5
mvykool <maykolandres4@gmail.com>
2024-10-18 07:49:11 -0500

    created page with editor

--- client/nebula/src/views/project/pages/page.tsx
+++ client/nebula/src/views/project/pages/page.tsx
@@ -1,7 +1,21 @@
+import { useCreateBlockNote } from "@blocknote/react";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useState } from "react";
+
 const Page = () => {
+  const [data, setData] = useState({
+    title: "title",
+    content: "",
+  });
+
+  const editor = useCreateBlockNote();
+
   return (
     <div className="w-full">
-      <h2>single page</h2>
+      <h2>{data.title}</h2>
+      <BlockNoteView editor={editor} data-theming-css-variables-demo />
     </div>
   );
 };
