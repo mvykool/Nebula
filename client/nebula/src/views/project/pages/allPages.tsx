cda2b55e5029453e611beb4c5084df5cdb1d9c2a
mvykool <maykolandres4@gmail.com>
2024-10-20 18:32:27 -0500

    added nagivate

--- client/nebula/src/views/project/pages/allPages.tsx
+++ client/nebula/src/views/project/pages/allPages.tsx
@@ -24,6 +24,7 @@ const AllPages = ({ pages, id }: Iprops) => {
       if (newPage && newPage.id) {
         console.log(newPage);
         console.log(`projects/${id}/pages/${newPage.id}`);
+        navigate(`pages/${newPage.id}`);
       }
     } catch (error) {
       console.log(error);
