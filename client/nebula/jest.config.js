45cb056bfdbc727deb0a5398a2d138fc7ce7e01a
mvykool <maykolandres4@gmail.com>
2024-10-27 00:11:50 -0500

    deleted jests tests

--- client/nebula/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
-  },
-  preset: "ts-jest",
-};
-
