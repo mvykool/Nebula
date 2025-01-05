655211072d7a3502976c6440a598efda330dac09
Maicol Hernandez <maykolandres4@gmail.com>
2025-01-05 14:37:10 -0500

    changed db from local to neon

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -18,13 +18,13 @@ import { imagesModule } from './images/images.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       entities: [User, Project, Page],
-      synchronize: true,
+      migrations: ['dist/migrations/*.js'],
+      synchronize: false,
     }),
     ThrottlerModule.forRoot([
       {
